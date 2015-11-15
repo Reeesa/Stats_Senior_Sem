8dea62b3e19cfe1c72728a4f1bb8d2985b19b5cb
Reeesa <brock245@morris.umn.edu>
2015-11-15 14:19:51 -0600

    Added some comments about get requests for the APIs

---
+++ src/getData/getBooks.java
@@ -0,0 +1,16 @@
+package getData;
+
+public class getBooks {
+	// API request format, NYT Bestsellers:
+	// GET http://api.nytimes.com/svc/books/v2/lists.json?list=hardcover-fiction&date=2015-11-08&api-key=KEY
+	// replace KEY with API key
+	// set date=_____  desired date of list in form YYYY-MM-DD
+	
+	// API request format, Google Bestsellers:
+	// GETGET https://www.googleapis.com/books/v1/volumes?q=TITLEKEYWORD+isbn=ISBN:keyes&key=KEY
+	// replace KEY with API key
+	// replace TITLEKEYWORD with title keyword
+	// replace ISBN with an isbn number
+	
+	// both api's will give results as a JSON object
+}
